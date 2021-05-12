d51ad3bdf3a12620594e51bc5110abfeef132340
Ashwin <ashwinrockon123@gmail.com>
2021-05-12 18:16:25 +0530

    Script working correctly, but html is loading after the JS code execution

---
+++ 2. Console Based Todo List/app.js
@@ -0,0 +1,36 @@
+let todoList = [];
+let run = true;
+
+while(run)
+{
+    let choice = prompt("What would you like to do?");
+    choice = choice.toLowerCase();
+    let todo;
+
+    switch(choice)
+    {
+        case "new" : todo = prompt("Enter new todo");
+                    todoList.push(todo);
+                    console.log(`${todo} added to the list`);
+        break;
+        
+        case "list" : console.log("****************");
+                    for(let i = 0; i < todoList.length; ++i)
+                    {
+                        console.log(`${i + 1}: ${todoList[i]}`);
+                    }
+                    console.log("****************");
+        break;
+
+        case "delete" : let deleteIdx = prompt("Enter the index of todo to delete");
+                        todoList.splice(--deleteIdx, 1);
+                        console.log("Todo Removed");
+        break;
+
+        case "quit" : run = false;
+                      console.log("Quitting.... Have a nice day ahead");
+        break;
+        
+        default : alert("Sorry, I didnt get you :(");
+    }
+}
