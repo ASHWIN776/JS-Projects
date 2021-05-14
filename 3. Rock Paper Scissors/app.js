ed51aacbb0c0862d4531d7830a99dde6e977a2ba
Ashwin <ashwinrockon123@gmail.com>
2021-05-14 22:42:03 +0530

    Fixed: user can enter no. of rounds

--- 3. Rock Paper Scissors/app.js
+++ 3. Rock Paper Scissors/app.js
@@ -7,6 +7,11 @@ const rules = {
   scissor: "paper"
 };
 
+const results = [0,0];
+const gamesCount = parseInt(prompt("How many rounds?"));
+console.log(game(gamesCount));
+
+// All functions
 function playRound(playerSelection, computerSelection) {
   if(playerSelection == computerSelection)
     return "TIE";
@@ -14,9 +19,12 @@ function playRound(playerSelection, computerSelection) {
   for(option in rules)
   {
     if(playerSelection === option && computerSelection === rules[option])
+    {
+      results[0]++;
       return `Haha..You win!! ${playerSelection} beats ${computerSelection}`; 
+    }
   }
-
+  results[1]++;
   return `Badluck, You lost!! ${computerSelection} beats ${playerSelection}`;
 }
   
@@ -28,6 +36,23 @@ function computerPlay()
     return game_options[idx];
 }
 
-const playerSelection = prompt("Rock Paper or Scissor?").toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function game(howMany)
+{ 
+  for(let i = 0; i < howMany; ++i)
+  {
+    const playerSelection = prompt("Rock Paper or Scissor?").toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  return "Result: " + displayResult();
+}
+
+function displayResult()
+{
+  console.log(`User Points = ${results[0]}, Computer Points = ${results[1]}`);
+
+  return results[0] < results[1] ? "Computer Wins the tournament" 
+  : results[0] > results[1] ? "You win the tournament"
+  : "Tournament Tied";
+}
