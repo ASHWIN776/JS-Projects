0913d69a73a185ff34fd7819598fc8d0453a366e
Ashwin <ashwinrockon123@gmail.com>
2021-05-14 21:35:40 +0530

    Script working, but can only play one game, the user can't select, playRound function is not clean!

---
+++ 3. Rock Paper Scissors/app.js
@@ -0,0 +1,20 @@
+const game_options = ["rock", "paper", "scissor"];
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection)
+      return "TIE";
+      
+    if((playerSelection == "rock" && computerSelection == "scissor") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissor" && computerSelection == "paper"))
+      return `Haha..You win!! ${playerSelection} beats ${computerSelection}`;
+    return `Badluck, You lost!! ${computerSelection} beats ${playerSelection}`;
+  }
+  
+function computerPlay()
+{
+    let idx = Math.floor(Math.random() * 3);
+    return game_options[idx];
+}
+
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
