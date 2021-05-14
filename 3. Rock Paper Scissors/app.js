f9a4f571bec9970e4f798060c4bec9e9b7dd9951
Ashwin <ashwinrockon123@gmail.com>
2021-05-14 22:14:01 +0530

    Fixed: playRound function enhanced

--- 3. Rock Paper Scissors/app.js
+++ 3. Rock Paper Scissors/app.js
@@ -1,20 +1,33 @@
 const game_options = ["rock", "paper", "scissor"];
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection == computerSelection)
-      return "TIE";
+// Key beats value
+const rules = {
+  rock: "scissor",
+  paper: "rock",
+  scissor: "paper"
+};
 
-    if((playerSelection == "rock" && computerSelection == "scissor") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissor" && computerSelection == "paper"))
-      return `Haha..You win!! ${playerSelection} beats ${computerSelection}`;
-    return `Badluck, You lost!! ${computerSelection} beats ${playerSelection}`;
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection == computerSelection)
+    return "TIE";
+
+  for(option in rules)
+  {
+    if(playerSelection === option && computerSelection === rules[option])
+      return `Haha..You win!! ${playerSelection} beats ${computerSelection}`; 
   }
+
+  return `Badluck, You lost!! ${computerSelection} beats ${playerSelection}`;
+}
   
 function computerPlay()
 {
     let idx = Math.floor(Math.random() * 3);
+    console.log(`Computer Selection : ${game_options[idx]}`);
+
     return game_options[idx];
 }
 
-const playerSelection = prompt("Rock Paper or Scissor?");
+const playerSelection = prompt("Rock Paper or Scissor?").toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
