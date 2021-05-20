57e657c9b0c3c6e435197d5f7eeed33cc3311914
Ashwin <ashwinrockon123@gmail.com>
2021-05-20 17:58:43 +0530

    Generating and showing numbers successfully, form validation remaining

---
+++ 7. Number Generator (0 - N)/scripts/app.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const numContainer = document.querySelector("#num");
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    // To reset the container -- otherwise it'll append numbers to it
+    numContainer.innerText = "";
+
+    const maxNum = parsInt(form.elements.maxNum.value);
+
+    if(maxNum > 1000) console.log("NOPE");
+    else
+        for(let i = 0; i < maxNum; ++i)
+            createElement(i);
+    
+});
+
+function createElement(num){
+    let reqClass;
+    
+    if(isPrime(num))
+        reqClass = "prime";
+    
+    else if(num % 2)
+        reqClass = "odd";
+
+    else reqClass = "even";
+
+    const span = document.createElement("span");
+    span.classList.add("numSpan", reqClass);
+    span.innerText = num;
+    numContainer.append(span);
+}
+
+function isPrime(num){
+    if(num <= 1) return false;
+
+    for(let i = 2; i <= num / 2; ++i)
+        if(num % i == 0) return false;
+    return true;
+}
+
