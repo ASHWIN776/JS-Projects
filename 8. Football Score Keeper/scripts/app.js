62e4ec24f6a71edc85dad39533483092571227c0
Ashwin <ashwinrockon123@gmail.com>
2021-05-21 14:15:30 +0530

    Introduced 2 variables for keeping track of team scores

--- 8. Football Score Keeper/scripts/app.js
+++ 8. Football Score Keeper/scripts/app.js
@@ -2,9 +2,11 @@ const team1 = document.querySelector("#team1");
 const team2 = document.querySelector("#team2");
 const reset = document.querySelector("#reset");
 const maxPointInput = document.querySelector("#maxP");
+const team1Display = document.querySelector("#team1Display");
+const team2Display = document.querySelector("#team2Display");
 
-let team1Score = document.querySelector("#team1Score");
-let team2Score = document.querySelector("#team2Score");
+let t1Score = 0;
+let t2Score = 0;
 // maxScore will default to 10
 let maxScore = parseInt(maxPointInput.value);
 
@@ -13,36 +15,35 @@ maxPointInput.addEventListener("input", (evt) => {
 });
 
 team1.addEventListener("click", () => {
-    team1Score.innerText = parseInt(team1Score.innerText) + 1;
-    check(parseInt(team1Score.innerText));
+    team1Display.innerText = ++t1Score;
 
     // To check Winning status
-    if(check(parseInt(team1Score.innerText)))
+    if(check(t1Score))
     {
-        team1Score.classList.add("win");
-        team2Score.classList.add("lose");
+        team1Display.classList.add("win");
+        team2Display.classList.add("lose");
         disableButtons();
     }
 });
 
 team2.addEventListener("click", () => {
-    team2Score.innerText = parseInt(team2Score.innerText) + 1;
-    console.log(parseInt(team2Score.innerText));
+    team2Display.innerText = ++t2Score;
 
      // To check Winning status
-    if(check(parseInt(team2Score.innerText)))
+    if(check(t2Score))
     {
-        team2Score.classList.add("win");
-        team1Score.classList.add("lose");
+        team2Display.classList.add("win");
+        team1Display.classList.add("lose");
         disableButtons();
     }
 });
 
 reset.addEventListener("click", () => {
-    team1Score.innerText = 0, team2Score.innerText = 0;
+    t1Score = 0, t2Score = 0;
+    team1Display.innerText = 0, team2Display.innerText = 0;
     enableButtons();
-    team1Score.className = "";
-    team2Score.className = "";
+    team1Display.className = "";
+    team2Display.className = "";
 });
 
 
