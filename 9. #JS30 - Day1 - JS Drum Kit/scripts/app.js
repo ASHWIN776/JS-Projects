7c4947d8d4645b589b08c0f40e2f5290830282af
Ashwin <ashwinrockon123@gmail.com>
2021-05-21 18:44:26 +0530

    Fixed: redundant code, changed keyup listener to transitionend, script working correctly

--- 9. #JS30 - Day1 - JS Drum Kit/scripts/app.js
+++ 9. #JS30 - Day1 - JS Drum Kit/scripts/app.js
@@ -1,22 +1,25 @@
+// Adding eventListener for keydown event
+window.addEventListener('keydown', (evt) => {
+    const key = document.querySelector(`.key[data-key="${evt.keyCode}"]`) 
+    const audio = document.querySelector(`audio[data-key="${evt.keyCode}"]`)
+    
+    if(!audio)  return;
+
+    audio.currentTime = 0 // to rewind to the start
+    audio.play(); // play the audio
+    key.classList.add("playing"); // add the playing class
+});
+
+
 const allKeys = document.querySelectorAll(".key");
 
-
-window.addEventListener('keydown', (evt) => {
-    for(let key of allKeys)
-    {
-        if(parseInt(key.dataset.key) === evt.keyCode)
-        {
-            key.classList.add("playing");
-        }
-    }
-});
-
-window.addEventListener('keyup', (evt) => {
-    for(let key of allKeys)
-    {
-        if(parseInt(key.dataset.key) === evt.keyCode)
-        {
-            key.classList.remove("playing");
-        }
-    }
-});
+// To remove the playing class after the transition(of transform) gets over
+for(let key of allKeys)
+{
+    key.addEventListener('transitionend', (evt) => {
+        // We gotta look only for transition for transforms
+        if(evt.propertyName !== "transform") return;
+        // Remove the playing class
+        key.classList.remove("playing");
+    });
+}
