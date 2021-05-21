abfb2f0f7c8bbe0d8edd4a55c61c8ec617ba8930
Ashwin <ashwinrockon123@gmail.com>
2021-05-21 14:41:43 +0530

    Added reset feature, when winning Score is changed

--- 8. Football Score Keeper/scripts/app.js
+++ 8. Football Score Keeper/scripts/app.js
@@ -1,6 +1,6 @@
-const team1 = document.querySelector("#team1");
-const team2 = document.querySelector("#team2");
-const reset = document.querySelector("#reset");
+const team1Btn = document.querySelector("#team1");
+const team2Btn = document.querySelector("#team2");
+const resetBtn = document.querySelector("#reset");
 const maxPointInput = document.querySelector("#maxP");
 const team1Display = document.querySelector("#team1Display");
 const team2Display = document.querySelector("#team2Display");
@@ -14,6 +14,7 @@ let maxScore = parseInt(maxPointInput.value);
 // Event listener to the dropdown
 maxPointInput.addEventListener("input", (evt) => {
     maxScore = parseInt(evt.target.value);
+    reset();
 });
 
 team1.addEventListener("click", () => {
@@ -42,19 +43,7 @@ team2.addEventListener("click", () => {
     }
 });
 
-reset.addEventListener("click", () => {
-    // reset scores
-    t1Score = 0, t2Score = 0;
-    team1Display.innerText = 0, team2Display.innerText = 0;
-
-    // Enable Buttons
-    enableButtons();
-
-    // Remove classes applied on scorecard
-    team1Display.className = "";
-    team2Display.className = "";
-});
-
+resetBtn.addEventListener("click", reset);
 
 function checkWinningStatus(score)
 {  
@@ -62,11 +51,24 @@ function checkWinningStatus(score)
 }
 
 function disableButtons(){
-    team1.disabled = true;
-    team2.disabled = true;
+    team1Btn.disabled = true;
+    team2Btn.disabled = true;
 }
 
 function enableButtons(){
-    team1.disabled = false;
-    team2.disabled = false;
+    team1Btn.disabled = false;
+    team2Btn.disabled = false;
+}
+
+function reset(){
+    // reset scores
+    t1Score = 0, t2Score = 0;
+    team1Display.innerText = 0, team2Display.innerText = 0;
+ 
+    // Enable Buttons
+    enableButtons();
+ 
+    // Remove classes applied on scorecard
+    team1Display.className = "";
+    team2Display.className = "";
 }
