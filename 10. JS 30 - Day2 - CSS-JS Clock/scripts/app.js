0cd25e09a9473e4321fc14153936c3321e5228a4
Ashwin <ashwinrockon123@gmail.com>
2021-05-23 01:02:19 +0530

    Scripts works correctly

---
+++ 10. JS 30 - Day2 - CSS-JS Clock/scripts/app.js
@@ -0,0 +1,21 @@
+const secHand = document.querySelector("#secHand");
+const minHand = document.querySelector("#minHand");
+const hrHand = document.querySelector("#hrHand");
+
+
+setInterval(() => {
+    const time = new Date();
+    const sec = time.getSeconds();
+    const min = time.getMinutes();
+    const hr = time.getHours();   
+
+    // At n seconds, rotate secHand (n * 6) if you consider 0deg to be at 12, but here 0deg is at 9
+    secHand.style.transform = `rotate(${90 + sec * 6}deg)`;
+    console.log(minHand.style.transform, 90 + (min * 60 + sec)*0.1);
+    
+    // 1 min = 6%, 1sec = 0.1deg
+    minHand.style.transform = `rotate(${90 + min * 6 + sec*0.1}deg)`;
+
+    // 1 hr === 30deg, 1min = 0.5deg and 1sec = 0.083deg
+    hrHand.style.transform = `rotate(${90 + hr * 30 + min*0.5 +  sec * 0.083}deg)`;
+},1000)
