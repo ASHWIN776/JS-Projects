b9dad3a192ca8c32ddddafc552ddebfec0656741
Ashwin <ashwinrockon123@gmail.com>
2021-05-26 00:16:30 +0530

    up and down content appears when open transition ends, fixed: infinite toggling of the active class

--- 13. JS30 - Day5 - Flex Panels Image Gallery/scripts/app.js
+++ 13. JS30 - Day5 - Flex Panels Image Gallery/scripts/app.js
@@ -1,7 +1,24 @@
 const panels = document.querySelectorAll(".panel");
 
+// Adds class "open" to the panel which is clicked
 function togglePanel(){
     this.classList.toggle("open");
 }
 
-panels.forEach(panel => panel.addEventListener("click", togglePanel))
+function toggleText(evt){
+    console.log(evt.propertyName)
+    // If I dont do this, then It will toggle the class infinitely
+    if(evt.propertyName === "flex-grow")
+    {
+        this.classList.toggle("active")
+    }
+}
+
+
+// Adding Eventlistener on each panel
+panels.forEach(panel => {
+    panel.addEventListener("click", togglePanel);
+
+    // After the flex-grow transition ends, we gotta show the content
+    panel.addEventListener("transitionend", toggleText)
+})
