723b19ee2fac6cbeea208c85fb705dffb1d9dd15
Ashwin <ashwinrockon123@gmail.com>
2021-06-01 14:44:09 +0530

    API data extracted successfully, using async; results for user's input wil be shown in the console

---
+++ 14. JS30 - Day6 - AJAX Type Ahead/scripts/app.js
@@ -0,0 +1,43 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const input = document.querySelector("input");
+
+let data;
+
+const getData = async function() {
+    try{
+        const res = await fetch(endpoint);
+        data = await res.json();
+        console.log("Got the data", data);
+    }
+    catch(err){
+        console.log("OOP!!! Failed - ", err);
+    }
+}
+
+// To get the data from the API
+getData();
+
+function getMatchedData(wordToMatch, data)
+{
+    const regex = new RegExp(wordToMatch, "gi");
+
+    return data.filter(place => 
+        (place.city.match(regex) || place.state.match(regex))
+    )
+}
+
+const displayMatchedData = function(){        
+    const wordToMatch = this.value;
+    if(!wordToMatch) return;
+
+    let matchedData = getMatchedData(wordToMatch, data);
+
+    matchedData = matchedData.map(place => ({
+        location: `${place.city}, ${place.state}`, 
+        population: place.population
+    }))
+    console.log(matchedData)
+}
+
+input.addEventListener("input", displayMatchedData)
