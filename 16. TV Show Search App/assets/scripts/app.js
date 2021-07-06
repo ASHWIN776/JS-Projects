3a10147d289b7553a9f72faa6ee67e2f2b3711e8
Ashwin <ashwinrockon123@gmail.com>
2021-07-06 22:22:23 +0530

    Added Comments

--- 16. TV Show Search App/assets/scripts/app.js
+++ 16. TV Show Search App/assets/scripts/app.js
@@ -1,3 +1,4 @@
+// API endpoint
 const endpoint =  "http://api.tvmaze.com/search/shows?q=";
 
 const searchForm = document.querySelector("#tvSearchForm");
@@ -10,13 +11,14 @@ async function showResults(evt)
     const query = searchForm.elements.showName.value;
 
     try{
+        // AJAX
         const res = await fetch(endpoint + query);
         const data = await res.json();
         console.log(data);
 
-
+        // HTML for query Results
         const results = data
-        .filter(({show:{image, url}}) => image && url)
+        .filter(({show:{image, url}}) => image && url)  
         .map(({show:{image:{medium}, url}}) => `
         <div class="img-container">
             <a href="${url}"><img src="${medium}"></a>
