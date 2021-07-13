7d8f341dbb26216e76ee37dcc3e1dd3d6521d8f2
Ashwin <ashwinrockon123@gmail.com>
2021-07-14 04:12:26 +0530

    Added event listeners to all checkboxes

--- 19. JS30 - Day10 - Hold shift to check multiple checkboxes/assets/scripts/app.js
+++ 19. JS30 - Day10 - Hold shift to check multiple checkboxes/assets/scripts/app.js
@@ -1,1 +1,12 @@
-console.log("Connected");
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+checkboxes.forEach(cb => cb.addEventListener("click", selectMultiple));
+
+function selectMultiple(e)
+{
+    console.log(e);
+    if(e.shiftKey && this.checked) // Hold shift === true && checkbox is checked
+    {
+        
+    }
+}
