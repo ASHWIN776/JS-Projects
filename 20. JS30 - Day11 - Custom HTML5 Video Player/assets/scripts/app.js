724aa57efd8433fa115daee5a5165b6a54da4831
Ashwin <ashwinrockon123@gmail.com>
2021-07-16 01:17:16 +0530

    Progress bar is working, still cannot move to certain time of video

--- 20. JS30 - Day11 - Custom HTML5 Video Player/assets/scripts/app.js
+++ 20. JS30 - Day11 - Custom HTML5 Video Player/assets/scripts/app.js
@@ -6,13 +6,33 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("button[data-skip]");
 const ranges = player.querySelectorAll("input[type='range']");
 
-let duration;
+let duration, videoWidth;
 
 // Takes time for the video metadata loads
 video.addEventListener("loadedmetadata", () => {
     duration = video.duration;
-    console.log(duration);
+    videoWidth = video.clientWidth - parseInt(getComputedStyle(progressBar).borderRightWidth);
+
+    console.log(duration, videoWidth);
+    progress.addEventListener("click", playHere);
+    setInterval(updateProgress, 1000);
 })
+
+function updateProgress()
+{
+    if(!video.paused)
+    {
+        pxInSec = 1 / (duration / videoWidth);
+        widthNow = video.currentTime * pxInSec;
+    
+        progressBar.style.flexBasis = `${widthNow}px`;
+    }
+}
+
+function playHere(e)
+{
+    console.log(e);
+}
 
 function togglePlay(e)
 {
@@ -27,5 +47,10 @@ toggle.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 
 // Updates Button
-video.addEventListener("play", e => toggle.innerHTML = "❚ ❚");
+video.addEventListener("play", e => 
+{
+    console.log(e);
+    toggle.innerHTML = "❚ ❚"
+}
+);
 video.addEventListener("pause", e => toggle.innerText = "►");
