ed529337661ceee54b501e5fa54cc71004f4ef6c
Ashwin <ashwinrockon123@gmail.com>
2021-07-16 00:37:31 +0530

    Play and pause video working

--- 20. JS30 - Day11 - Custom HTML5 Video Player/assets/scripts/app.js
+++ 20. JS30 - Day11 - Custom HTML5 Video Player/assets/scripts/app.js
@@ -1,1 +1,23 @@
-console.log("Connected");
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("button[data-skip]");
+const ranges = player.querySelectorAll("input[type='range']");
+
+function togglePlay(e)
+{
+    if(video.paused)
+        video.play();
+
+    else 
+        video.pause();
+}
+
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+
+// Updates Button
+video.addEventListener("play", e => toggle.innerHTML = "❚ ❚");
+video.addEventListener("pause", e => toggle.innerText = "►");
