6e4e274f48eb415420aeda9c69421db5032e38a3
Ashwin <ashwinrockon123@gmail.com>
2021-07-16 01:31:11 +0530

    Can move to any other time of the video using progressBar

--- 20. JS30 - Day11 - Custom HTML5 Video Player/assets/scripts/app.js
+++ 20. JS30 - Day11 - Custom HTML5 Video Player/assets/scripts/app.js
@@ -6,15 +6,18 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("button[data-skip]");
 const ranges = player.querySelectorAll("input[type='range']");
 
-let duration, videoWidth;
+let duration, videoWidth, pxInSec;
 
 // Takes time for the video metadata loads
 video.addEventListener("loadedmetadata", () => {
     duration = video.duration;
     videoWidth = video.clientWidth - parseInt(getComputedStyle(progressBar).borderRightWidth);
+    pxInSec = 1 / (duration / videoWidth); // Pixels to move in 1s
 
     console.log(duration, videoWidth);
     progress.addEventListener("click", playHere);
+
+    // Updates the progressBar in intervals of 1s
     setInterval(updateProgress, 1000);
 })
 
@@ -22,16 +25,20 @@ function updateProgress()
 {
     if(!video.paused)
     {
-        pxInSec = 1 / (duration / videoWidth);
-        widthNow = video.currentTime * pxInSec;
-    
+        const widthNow = video.currentTime * pxInSec; // the width in pixels that the progressBar should have at a specific video.currentTime
+
         progressBar.style.flexBasis = `${widthNow}px`;
     }
 }
 
 function playHere(e)
 {
-    console.log(e);
+    // This will give the mouse coordinates relative to the element, as theres no padding
+    const mouseCoordX = e.offsetX;
+    // This will give the video.currentTime at this width
+    const timeNow = mouseCoordX / pxInSec;
+    progressBar.style.flexBasis = `${mouseCoordX}px`;
+    video.currentTime = timeNow;
 }
 
 function togglePlay(e)
