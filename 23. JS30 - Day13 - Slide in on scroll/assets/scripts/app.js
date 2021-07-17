45daf45f61492e8ce7def4dd0d14b7633cf3f67f
Ashwin <ashwinrockon123@gmail.com>
2021-07-18 01:26:10 +0530

    JS working

--- 23. JS30 - Day13 - Slide in on scroll/assets/scripts/app.js
+++ 23. JS30 - Day13 - Slide in on scroll/assets/scripts/app.js
@@ -1,1 +1,48 @@
-console.log("Connected");
+const images = document.querySelectorAll(".slide-in");
+
+// Debounce fn => this runs the slideIn waiting 20ms
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+};
+
+
+window.addEventListener("scroll", debounce(slideIn));
+
+function slideIn(e)
+{
+    images.forEach(img => {
+        // Check if the image is in the page or not?
+
+        // When to slide the image in?
+        const slideInAt = window.scrollY + window.innerHeight - img.height / 2;
+
+        // When to slide the image back out ?
+        const slideOutAt = img.offsetTop + img.height;
+
+        console.log(img.offsetTop, slideInAt);
+
+        // Add or remove the "active" class
+        const isHalfShown = slideInAt > img.offsetTop;
+        const isNotScrolledPast = window.scrollY < slideOutAt;
+
+        // The image should be slided in if half of the image is shown and the whole image is not scrolled past
+        if(isHalfShown && isNotScrolledPast)
+            img.classList.add("active");
+        
+        // If the image is scrolled Past || image half aint shown, remove the class(active)
+        else 
+            img.classList.remove("active");
+    })
+    console.log("****");
+}
