ec91269a3b4a8e9ade8ed606ff8f3a90ed14ca43
Ashwin <ashwinrockon123@gmail.com>
2021-07-20 03:08:25 +0530

    Item submitted in the form is shown in the plates div

--- 25. JS30 - Day15 - Local Storage and Event Delegation/assets/scripts/app.js
+++ 25. JS30 - Day15 - Local Storage and Event Delegation/assets/scripts/app.js
@@ -1,1 +1,31 @@
-const itemsContainer = document.querySelector("add-items");
+const itemForm = document.querySelector("#itemContainer");
+const plates = document.querySelector(".plates");
+const items = [];
+
+// Getting the items
+itemForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const value = itemForm.querySelector('input[name="item"]').value;
+
+    const item = {
+        text : value,
+        done: false
+    };
+    items.push(item);
+    // Resetting the inputs
+    itemForm.reset();
+    populatePlates();
+})
+
+function populatePlates()
+{
+    if(items.length)
+    {
+        let listHTML = items.map(item => `<li>
+        <input type="checkbox"> ${item.text} 
+        </li> `).join("");
+        console.log(listHTML);
+
+        plates.innerHTML = listHTML;
+    }
+}
